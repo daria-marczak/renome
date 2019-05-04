d37266f1907412a37e94e5cf3952abdf23bbba16
Daria Marczak <marczak.ria@gmail.com>
2019-05-04 16:04:00 +0200

    setup project with all needed dependencies

---
+++ src/assets/styles/GlobalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: "Axis";
+        src: url("../fonts/axis.otf") format("otf");
+    }
+`;
+
+export default GlobalStyle;
---
+++ src/assets/styles/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  font: {
+    family: {
+      montserrat: 'Montserrat, sans-serif',
+      axis: 'Axis, sans-serif',
+    },
+  },
+};
