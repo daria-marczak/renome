9e08c6390c072e326a37a2dc20d0fbad7718b36d
Daria Marczak <marczak.ria@gmail.com>
2019-05-18 15:25:07 +0200

    Images for carousel are showing

--- src/components/common/Carousel.js
+++ src/components/common/Carousel.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Img from 'gatsby-image';
 
@@ -9,31 +10,10 @@ const StyledWrapper = styled.div`
   overflow: 'hidden';
 `;
 
-const Carousel = () => {
+const Carousel = ({ data }) => {
   const [index, setIndex] = useState(0);
 
-  const { allFile } = useStaticQuery(graphql`
-    query {
-      allFile(
-        sort: { fields: name, order: DESC }
-        filter: { relativeDirectory: { eq: "carousel" } }
-      ) {
-        edges {
-          node {
-            id
-            name
-            childImageSharp {
-              fluid(maxWidth: 600) {
-                ...GatsbyImageSharpFluid_noBase64
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  const length = allFile.edges.length - 1;
+  const length = data && data.allFile.edges.length - 1;
 
   const handleChange = isNext =>
     index === length
@@ -44,11 +24,11 @@ const Carousel = () => {
       ? setIndex(index + 1)
       : setIndex(index - 1);
 
-  const { node } = allFile.edges[index];
-
   return (
     <StyledWrapper>
-      <Img fluid={node.childImageSharp.fluid} key={node.id} alt={node.name} />
+      {data.allFile.edges.map(({ node }) => (
+        <Img fluid={node.childImageSharp.fluid} key={node.id} alt={node.name} />
+      ))}
 
       <button type="button" onClick={() => handleChange(true)}>
         Previous
@@ -60,4 +40,30 @@ const Carousel = () => {
   );
 };
 
-export default Carousel;
+// eslint-disable-next-line react/display-name
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allFile(filter: { absolutePath: { regex: "/carousel/" } }) {
+          edges {
+            node {
+              id
+              name
+              childImageSharp {
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Carousel data={data} {...props} />}
+  />
+);
+
+Carousel.propTypes = {
+  data: PropTypes.shape().isRequired,
+};
