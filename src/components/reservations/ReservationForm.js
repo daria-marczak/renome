f824d500ac082d4c63284048379459379acba8c4
Daria Marczak <marczak.ria@gmail.com>
2019-06-21 19:06:03 +0200

    fixed styles for desktop

--- src/components/reservations/ReservationForm.js
+++ src/components/reservations/ReservationForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import 'react-dates/initialize';
+// import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { graphql, StaticQuery } from 'gatsby';
@@ -12,6 +14,12 @@ const StyledHeading = styled.h4`
   text-align: center;
 `;
 
+const StyledReservationWrapper = styled(StyledWrapper)`
+  @media (min-width: 1200px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
 const StyledButton = styled.button`
   background: #1e2633;
   text-transform: uppercase;
@@ -19,17 +27,35 @@ const StyledButton = styled.button`
   font-size: ${({ theme }) => theme.font.size.paragraph};
   padding: 20px 20px;
   border: none;
-  width: 80%;
+  width: 100%;
   margin: 15px 0 20px 0;
   font-family: ${({ theme }) => theme.font.family.montserrat};
   font-weight: 600;
   align-self: center;
+
+  @media (min-width: 1200px) {
+    width: auto;
+  }
 `;
 
 const StyledForm = styled.form`
+  font-family: ${({ theme }) => theme.font.family.montserrat};
+
+  @media (min-width: 1200px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const StyledFormWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  font-family: ${({ theme }) => theme.font.family.montserrat};
+  flex: 1;
+
+  @media (min-width: 1200px) {
+    flex-direction: row;
+    justify-content: center;
+  }
 `;
 
 const StyledImage = styled(Img)`
@@ -41,7 +67,7 @@ const StyledInput = styled.input`
   border: none;
   font-family: ${({ theme }) => theme.font.family.montserrat};
   padding: 15px 10px;
-  border-bottom: 1px solid ${({ theme }) => theme.lightGray};
+  border-bottom: 3px solid ${({ theme }) => theme.lightGray};
   margin-left: 10px;
   flex: 1;
 `;
@@ -49,6 +75,15 @@ const StyledInput = styled.input`
 const StyledLabel = styled.label`
   display: flex;
   align-items: center;
+
+  @media (min-width: 1200px) {
+    width: 200px;
+    margin-left: 10px;
+
+    &:first-of-type {
+      margin-left: 0;
+    }
+  }
 `;
 
 const ReservationForm = ({ data }) => {
@@ -68,50 +103,52 @@ const ReservationForm = ({ data }) => {
   };
 
   return (
-    <StyledWrapper>
+    <StyledReservationWrapper>
       <StyledHeading>make a reservation</StyledHeading>
       <StyledForm onSubmit={handleSubmit}>
-        <StyledLabel htmlFor="reservationDate">
-          <StyledImage fluid={data.calendar.childImageSharp.fluid} />
-          <StyledInput
-            type="date"
-            id="reservationDate"
-            name="reservation-date"
-            value={date}
-            min={today}
-            max={nextTwoWeeks}
-            onChange={event => setDate(event.target.value)}
-            required
-          />
-        </StyledLabel>
-        <StyledLabel htmlFor="reservationTime">
-          <StyledImage fluid={data.hours.childImageSharp.fluid} />
-          <StyledInput
-            type="time"
-            id="reservationTime"
-            name="reservation-time"
-            min="9:00"
-            max="17:00"
-            value={time}
-            onChange={event => setTime(event.target.value)}
-            required
-          />
-        </StyledLabel>
-        <StyledLabel htmlFor="peopleAmount">
-          <StyledImage fluid={data.people.childImageSharp.fluid} />
-          <StyledInput
-            type="number"
-            min="1"
-            max="10"
-            id="peopleAmount"
-            value={people}
-            onChange={event => setPeopleAmount(event.target.value)}
-            required
-          />
-        </StyledLabel>
+        <StyledFormWrapper>
+          <StyledLabel htmlFor="reservationDate">
+            <StyledImage fluid={data.calendar.childImageSharp.fluid} />
+            <StyledInput
+              type="date"
+              id="reservationDate"
+              name="reservation-date"
+              value={date}
+              min={today}
+              max={nextTwoWeeks}
+              onChange={event => setDate(event.target.value)}
+              required
+            />
+          </StyledLabel>
+          <StyledLabel htmlFor="reservationTime">
+            <StyledImage fluid={data.hours.childImageSharp.fluid} />
+            <StyledInput
+              type="time"
+              id="reservationTime"
+              name="reservation-time"
+              min="9:00"
+              max="17:00"
+              value={time}
+              onChange={event => setTime(event.target.value)}
+              required
+            />
+          </StyledLabel>
+          <StyledLabel htmlFor="peopleAmount">
+            <StyledImage fluid={data.people.childImageSharp.fluid} />
+            <StyledInput
+              type="number"
+              min="1"
+              max="10"
+              id="peopleAmount"
+              value={people}
+              onChange={event => setPeopleAmount(event.target.value)}
+              required
+            />
+          </StyledLabel>
+        </StyledFormWrapper>
         <StyledButton type="submit">Find a table</StyledButton>
       </StyledForm>
-    </StyledWrapper>
+    </StyledReservationWrapper>
   );
 };
 
