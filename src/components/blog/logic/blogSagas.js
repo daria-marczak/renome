d37b64def66fbf797226748332b17d22e4c92eec
Daria Marczak <marczak.ria@gmail.com>
2019-06-28 17:15:31 +0200

    adding comments works

--- src/components/blog/logic/blogSagas.js
+++ src/components/blog/logic/blogSagas.js
@@ -1,18 +1,30 @@
-import { takeLatest, put } from 'redux-saga/effects';
+import { takeLatest, put, delay } from 'redux-saga/effects';
 import * as blogActions from './blogConstants';
 
 export function* blogSagas() {
   yield takeLatest(blogActions.CREATE_COMMENT.INVOKE, createComment);
 }
 
-function* createComment(commentData) {
-  yield put({ type: blogActions.CREATE_COMMENT.REQUEST, commentData });
+function* createComment({ comment }) {
+  yield put({ type: blogActions.CREATE_COMMENT.REQUEST, comment });
 
   try {
     const response = { isSuccess: true };
 
     if (response.isSuccess) {
-      yield put({ type: blogActions.CREATE_COMMENT.SUCCESS });
+      const commentContent = {
+        id: Math.random(),
+        author: comment.author,
+        email: comment.email,
+        date: new Date(),
+        content: comment.message,
+      };
+
+      yield delay(1500);
+      yield put({
+        type: blogActions.CREATE_COMMENT.SUCCESS,
+        comment: commentContent,
+      });
     } else {
       yield put({ type: blogActions.CREATE_COMMENT.FAILURE });
     }
