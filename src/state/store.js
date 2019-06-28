2a795cdbf58bcae84b06dd1ca05f0d84b4ed7794
Daria Marczak <marczak.ria@gmail.com>
2019-06-28 16:27:31 +0200

    saga is now connected

--- src/state/store.js
+++ src/state/store.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore as reduxCreateStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './rootReducer';
-
-const createStore = () =>
-  reduxCreateStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+import rootSaga from './rootSaga';
 
 // eslint-disable-next-line react/prop-types
 export default ({ element }) => {
-  return <Provider store={createStore()}>{element}</Provider>;
+  const sagaMiddleware = createSagaMiddleware();
+
+  const middleware = [sagaMiddleware];
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const enhancer = composeEnhancers(applyMiddleware(...middleware));
+  const store = createStore(rootReducer, {}, enhancer);
+
+  sagaMiddleware.run(rootSaga);
+
+  return <Provider store={store}>{element}</Provider>;
 };
