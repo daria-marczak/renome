eb2a207add0bd6fea2de8e4afbbb4f1ab4cdf3af
Daria Marczak <marczak.ria@gmail.com>
2019-07-07 12:03:43 +0200

    snackbar connected to all sagas

--- src/components/checkout/logic/checkoutSagas.js
+++ src/components/checkout/logic/checkoutSagas.js
@@ -1,5 +1,6 @@
 import { takeLatest, put, delay, select } from 'redux-saga/effects';
 import * as checkoutActions from './checkoutConstants';
+import * as appActions from '../../../appLogic/appConstants';
 
 const productSelector = state => state.cart.cartItems;
 const paymentSelector = state => state.checkout.paymentMethod;
@@ -22,8 +23,12 @@ function* placeOrder(action) {
 
       const products = yield select(productSelector);
       const paymentMethod = yield select(paymentSelector);
-
       yield delay(1500);
+      yield put({
+        type: appActions.TRIGGER_MESSAGE,
+        kind: 'success',
+        message: 'Success! Your order was placed',
+      });
       yield put({
         type: checkoutActions.CHECKOUT_PRODUCTS.SUCCESS,
         addresses,
@@ -31,9 +36,19 @@ function* placeOrder(action) {
         paymentMethod,
       });
     } else {
+      yield put({
+        type: appActions.TRIGGER_MESSAGE,
+        kind: 'success',
+        message: 'Something went wrong',
+      });
       yield put({ type: checkoutActions.CHECKOUT_PRODUCTS.FAILURE });
     }
   } catch (error) {
+    yield put({
+      type: appActions.TRIGGER_MESSAGE,
+      kind: 'error',
+      message: 'Something went wrong',
+    });
     yield put({ type: checkoutActions.CHECKOUT_PRODUCTS.FAILURE });
   }
 }
--- src/components/shop/logic/shopSagas.js
+++ src/components/shop/logic/shopSagas.js
@@ -1,5 +1,6 @@
 import { takeLatest, put, delay } from 'redux-saga/effects';
 import * as shopActions from './shopConstants';
+import * as appActions from '../../../appLogic/appConstants';
 
 export function* shopSagas() {
   yield takeLatest(shopActions.ADD_REVIEW.INVOKE, addReview);
@@ -21,13 +22,28 @@ function* addReview({ reviewContent }) {
 
       yield delay(1500);
       yield put({
+        type: appActions.TRIGGER_MESSAGE,
+        kind: 'success',
+        message: 'Review added correctly',
+      });
+      yield put({
         type: shopActions.ADD_REVIEW.SUCCESS,
         comment: commentContent,
       });
     } else {
+      yield put({
+        type: appActions.TRIGGER_MESSAGE,
+        kind: 'success',
+        message: 'Something went wrong',
+      });
       yield put({ type: shopActions.ADD_REVIEW.FAILURE });
     }
   } catch (error) {
+    yield put({
+      type: appActions.TRIGGER_MESSAGE,
+      kind: 'success',
+      message: 'Something went wrong',
+    });
     yield put({ type: shopActions.ADD_REVIEW.FAILURE });
   }
 }
