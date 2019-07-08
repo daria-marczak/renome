9b3ae77d7cb088c0eb7daa724b1887ff4fcf89f9
Daria Marczak <marczak.ria@gmail.com>
2019-07-08 16:42:49 +0200

    fixing deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   pathPrefix: '/renome',
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
