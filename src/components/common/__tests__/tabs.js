6403534160a00cd4967e49943109143712bee831
Daria Marczak <marczak.ria@gmail.com>
2019-07-21 12:07:26 +0200

    tabs tested

--- src/components/common/__tests__/tabs.js
+++ src/components/common/__tests__/tabs.js
@@ -1,33 +1,27 @@
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
 import { render } from '@testing-library/react';
 import Tabs from '../Tabs';
 import theme from '../../../assets/styles/theme';
 import * as tabs from '../__mocks__/tabs';
 
 jest.mock('./tabs');
+const setTab = jest.fn;
 
 describe('Tabs', () => {
-  it('renders correctly', () => {
-    const tree = new ShallowRenderer(
-      <Tabs theme={theme} tabs={tabs.tabs} activeTab="Breakfast" />
-    );
-    expect(tree).toMatchSnapshot();
-  });
+  const { getByTestId, container } = render(
+    <Tabs
+      theme={theme}
+      tabs={tabs.tabs}
+      activeTab="Breakfast"
+      setTab={setTab}
+    />
+  );
 
-  it('has active tab with underline', () => {
-    const { getByTestId } = render(
-      <Tabs theme={theme} tabs={tabs.tabs} activeTab="Breakfast" />
-    );
-    expect(getByTestId('activeTab')).toHaveStyle(
-      `border-bottom: 5px solid #985e5b`
-    );
+  it('renders component', () => {
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('has active tab with correct text', () => {
-    const { querryAllById } = render(
-      <Tabs theme={theme} tabs={tabs.tabs} activeTab="Breakfast" />
-    );
-    expect(querryAllById('activeTab')).toHaveTextContent('Breakfast');
+    expect(getByTestId('activeTab').innerHTML).toBe('Breakfast');
   });
 });
